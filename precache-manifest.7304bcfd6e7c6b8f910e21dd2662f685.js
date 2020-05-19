8d3707b3726ddcc33357961ce99dd3474998b41d
peter <peter01twn@gmail.com>
2020-05-19 14:43:12 +0800

    Updates

---
+++ precache-manifest.7304bcfd6e7c6b8f910e21dd2662f685.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6171735ffa195fdd7bc2ec72a1c4d0a",
+    "url": "/github-repo-view/index.html"
+  },
+  {
+    "revision": "d08dc26a6f887600ef0d",
+    "url": "/github-repo-view/static/css/main.9c23427c.chunk.css"
+  },
+  {
+    "revision": "c552020315bf6a99d229",
+    "url": "/github-repo-view/static/js/2.83a04da1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-repo-view/static/js/2.83a04da1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d08dc26a6f887600ef0d",
+    "url": "/github-repo-view/static/js/main.3a174735.chunk.js"
+  },
+  {
+    "revision": "4339136d859bf4311b2d",
+    "url": "/github-repo-view/static/js/runtime-main.78ae5178.js"
+  }
+]);
